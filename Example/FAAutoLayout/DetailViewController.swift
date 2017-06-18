7fc07bb785ff2600828fabb542e49bce2cf396d5
Fábio Almeida <fabio.andre.almeida@gmail.com>
2017-06-18 22:44:56 +0100

    Updated README and added more samples to the Example project

---
+++ Example/FAAutoLayout/DetailViewController.swift
@@ -0,0 +1,47 @@
+//
+//  DetailViewController.swift
+//  FAAutoLayout
+//
+//  Created by Fábio Almeida on 18/06/2017.
+//  Copyright (c) 2017 Fábio Almeida. All rights reserved.
+//
+
+import UIKit
+import FAAutoLayout
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.addSubviews()
+    }
+
+    // MARK: - Private
+
+    private func addSubviews() {
+
+        let dynamicNumberOfViews = 5
+        var dynamicViews = [UIView]()
+
+        for _ in 0..<dynamicNumberOfViews {
+
+            let newView = UIView()
+            newView.backgroundColor = UIColor.blue
+            dynamicViews.append(newView)
+            self.view.addSubview(newView)
+
+            // add constraints
+            newView.constrainHeight(50.0)
+            newView.constrainTopSpaceToContainer(15.0)
+        }
+
+        // as you can see here, these methods are not called on some UIView instance but are UIView class methods
+        UIView.constrainEqualWidth(dynamicViews)
+        UIView.constrainEqualHorizontalSpacing(dynamicViews, constant: 30.0)
+
+        dynamicViews.first?.constrainLeadingSpaceToContainer(15.0)
+        dynamicViews.last?.constrainTrailingSpaceToContainer(15.0)
+    }
+
+}
--- Example/FAAutoLayout/ViewController.swift
+++ Example/FAAutoLayout/ViewController.swift
@@ -17,43 +17,67 @@ class ViewController: UIViewController {
         self.addSubviews()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
     // MARK: Private
 
     private func addSubviews() {
 
-        // initial view, ajusting container view with a padding of 25pt on all sides
-        let view1 = UIView()
-        view1.backgroundColor = UIColor.lightGray
-        self.view.addSubview(view1)
-        view1.fillContainer(25.0)
+        // background view that adjusts the container view without any padding
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = UIColor.lightGray
+        self.view.addSubview(backgroundView)
+        self.view.sendSubview(toBack: backgroundView)
+
+        backgroundView.fillContainer()
 
 
-        // view2 which sits on the top-left corner of view1, with a top padding of 10pt
-        let view2 = UIView()
-        view2.backgroundColor = UIColor.blue
-        view1.addSubview(view2)
+        // view which sits on the top-left corner of the background view, with a fixed width and height
+        let topLeftView = UIView()
+        topLeftView.backgroundColor = UIColor.blue
+        backgroundView.addSubview(topLeftView)
 
-        view2.constrainLeadingSpaceToContainer()
-        view2.constrainTopSpaceToContainer(10.0)
-        view2.constrainWidth(150.0)
-        view2.constrainHeight(45.0)
+        topLeftView.constrainLeadingSpaceToContainer()
+        topLeftView.constrainTopSpaceToContainer(10.0)
+        topLeftView.constrainWidth(150.0)
+        topLeftView.constrainHeight(45.0)
 
-        // view3, sitting right bellow view2
-        let view3 = UIView()
-        view3.backgroundColor = UIColor.red
-        view1.addSubview(view3)
 
-        view3.constrainLeadingSpaceToContainer(10.0)
-        view3.constrainVerticalSpacing(toView: view2, constant: 5.0)
-        view3.constrainWidth(100.0)
-        view3.constrainHeight(20.0)
+        // view that is positioned on the right on relation with topLeftView
+        let topRightView = UIView()
+        topRightView.backgroundColor = UIColor.red
+        backgroundView.addSubview(topRightView)
 
+        topRightView.constrainTopSpaceToContainer(10.0)
+        topRightView.constrainTrailingSpaceToContainer(30.0)
+        topRightView.constrainHorizontalSpacing(toView: topLeftView, constant: 20.0)
+        topRightView.constrainEqualHeight(toView: topLeftView, constant: 0, relation: .equal, priority: UILayoutPriorityRequired, multiplier: 2.0)
+
+
+        // container view that will sit below topLeftView
+        let containerView1 = UIView()
+        containerView1.backgroundColor = UIColor.green
+        backgroundView.addSubview(containerView1)
+
+        containerView1.constrainWidth(200.0)
+        containerView1.constrainHeight(200.0)
+        containerView1.constrainVerticalSpacing(toView: topLeftView, constant: 100.0)
+        containerView1.constrainLeadingSpaceToContainer(10.0)
+
+
+        // view that will adapt to containerView1
+        let containerView2 = UIView()
+        containerView2.backgroundColor = UIColor.black
+        containerView1.addSubview(containerView2)
+
+        containerView2.fillContainer(40.0)
+
+
+        // view that that despite being inside containerView2, will adapt to containerView1
+        let containerView3 = UIView()
+        containerView3.backgroundColor = UIColor.orange
+        containerView3.alpha = 0.5
+        containerView2.addSubview(containerView3)
+
+        containerView3.fill(view: containerView1, constant: 20.0)
     }
 }
 
